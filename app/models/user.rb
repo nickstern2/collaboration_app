e515985dd25e5d92220cd0f962914e23c5698f5e
nick stern <nicholas.stern2@gmail.com>
2019-09-27 19:07:46 +0800

    matches work, bitch! -fingers crossed

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          has_many :likes
+
+
+
+         def full_name
+            self.first_name + " " + self.last_name
+         end
 end
