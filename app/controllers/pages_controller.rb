ae08be2acc495adfd8228dd61b836e14259b3abc
nick stern <nicholas.stern2@gmail.com>
2019-09-28 18:34:42 +0800

    no change

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-
-    # @users = User.all
     if current_user.nil?
       redirect_to new_user_session_path
     end
