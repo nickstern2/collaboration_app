cb313a51024e2b16be65575be92540283ab67e0c
nick stern <nicholas.stern2@gmail.com>
2019-10-25 17:00:22 -0400

    unmatch works + validation check for single like + likes page no longer shows people who you liked=false + ...maybe more

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,15 +1,20 @@
 class Like < ApplicationRecord
   belongs_to :user, dependent: :destroy
+  validates :swiped_id, :uniqueness => { :scope => :user_id}
 
-# Do I need these with my current logic set-up?
   validates :user_id, presence: true
   validates :swiped_id, presence: true
+
+
   # after_create :check_for_match
 
-  def check_for_match
-    unless Like.where(user_id: self.swiped_id, swiped_id: self.user_id, liked: true).empty?
-      Match.create(user_two_id: self.swiped_id )
-    end
-  end
+  # def check_for_match
+  #   unless Like.where(user_id: self.swiped_id, swiped_id: self.user_id, liked: true).empty?
+  #     Match.create(user_two_id: self.swiped_id )
+  #   end
+  # end
 
+  def delete_duplicate_likes
+    user_likes = Like.where(user_id: self.id, )
+  end
 end
